c6bd86fadfd56e8764523329ad595635adc7ada4
Kanchan Kalauni <kanchankalauni03@gmail.com>
2024-07-11 16:50:26 +0530

    Edit Note

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-let txtA = document.getElementById('txtA')
-let div = document.getElementById('div')
+
 
 document.getElementById('btn').addEventListener('click', () => {
-    div.innerHTML = txtA.value
-    txtA.classList.toggle('hidden')
-    div.classList.toggle('hidden')
+
+    let divEle = document.createElement('div')
+    divEle.setAttribute('class', 'singleNote')
+    divEle.innerHTML = `
+    <div>
+        <button class="editBtn">Edit</button>
+        <button class="removeBtn">Remove</button>
+    </div>
+    <div>
+        <div class="note hidden" id="div"></div>
+        <textarea class="note " id="txtA"></textarea>
+    </div>
+    <div class="date">04/06/2024</div>
+    `
+
+    let editBtn = divEle.querySelector('.editBtn')
+    
+    editBtn.addEventListener('click', () => {
+        let txtA = divEle.querySelector('#txtA')
+        let div = divEle.querySelector('#div')
+        // console.log(txtA)
+        div.innerHTML = txtA.value
+        txtA.classList.toggle('hidden')
+        div.classList.toggle('hidden')
+    })
+
+    document.getElementById('allNote').append(divEle)
+    
 })
