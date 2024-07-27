4b3c36568d6b2be92a230f274aa7ecbe10fc167e
Kanchan Kalauni <kanchankalauni03@gmail.com>
2024-07-27 13:41:58 +0530

    message

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+let data = JSON.parse(localStorage.getItem('notes')) || []
+
+function displayNote() {
+    data.map((singleNote) => {
+        // console.log(singleNote)
+        if (singleNote) {
+            addNote(singleNote)
+        }
+    })
+}
+
+displayNote()
+
 document.getElementById('btn').addEventListener('click', () => {
     addNote()
 })
 
-function addNote() {
+function addNote(note = '') {
+    console.log(note)
     let date
     function setDate() {
         date = new Date().toLocaleString()
@@ -16,18 +30,21 @@ function addNote() {
         <button class="removeBtn">Remove</button>
     </div>
     <div>
-        <div class="note hidden" id="div"></div>
-        <textarea class="note " id="txtA"></textarea>
+        <div class="note ${note ? '' : 'hidden'}" id="div"></div>
+        <textarea class="note ${note ? 'hidden' : ''}" id="txtA"></textarea>
     </div>
     <div class="date" id="dateDiv">${date}</div>
     `
 
     let editBtn = divEle.querySelector('.editBtn')
     let removeBtn = divEle.querySelector('.removeBtn')
+    let txtA = divEle.querySelector('#txtA')
+    let div = divEle.querySelector('#div')
+
+    div.innerHTML = note
+    txtA.innerHTML = note
     
     editBtn.addEventListener('click', () => {
-        let txtA = divEle.querySelector('#txtA')
-        let div = divEle.querySelector('#div')
         let dateDiv = divEle.querySelector('#dateDiv')
         // console.log(txtA)
         div.innerHTML = txtA.value
@@ -39,8 +56,25 @@ function addNote() {
 
     removeBtn.addEventListener('click', (e) => {
         divEle.remove()
+        updateStorage()
         // e.target.parentNode.parentNode.remove()
     })
 
+    txtA.addEventListener('input' , () => {
+        updateStorage()
+    })
+
     document.getElementById('allNote').append(divEle)
+}
+
+function updateStorage() {
+    let textArea = document.querySelectorAll('textarea')
+    data = []
+    textArea.forEach(e => {
+        if (e) {
+            data.push(e.value)   
+        }
+    })
+    localStorage.setItem('notes', JSON.stringify(data))
+    // displayNote()
 }
