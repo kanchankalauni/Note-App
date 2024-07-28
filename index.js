d5e1406ba4b19f02a47f596d5ab71686a643c78c
Kanchan Kalauni <kanchankalauni03@gmail.com>
2024-07-28 22:25:13 +0530

    Optimise

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let data = JSON.parse(localStorage.getItem('notes')) || []
+
 function displayNote() {
     data.map((singleNote) => {
-        // console.log(singleNote)
         if (singleNote) {
             addNote(singleNote)
         }
@@ -16,7 +16,6 @@ document.getElementById('btn').addEventListener('click', () => {
 
 function addNote(note = {}) {
     const {title = "", noteDate = setDate()} = note
-    
     
     let divEle = document.createElement('div')
     divEle.setAttribute('class', 'singleNote')
@@ -36,14 +35,11 @@ function addNote(note = {}) {
     let removeBtn = divEle.querySelector('.removeBtn')
     let txtA = divEle.querySelector('#txtA')
     let div = divEle.querySelector('#div')
-    // let dateDiv = divEle.querySelector('#dateDiv')
 
     div.innerHTML = title
     txtA.innerHTML = title
-    // dateDiv.innerHTML = note.noteDate
     
     editBtn.addEventListener('click', () => {
-        // console.log(txtA)
         let isEditing = editBtn.textContent === 'Edit'
         editBtn.textContent = isEditing ? 'Save' : 'Edit'
         div.innerHTML = txtA.value
@@ -51,7 +47,7 @@ function addNote(note = {}) {
         div.classList.toggle('hidden')
     })
 
-    removeBtn.addEventListener('click', (e) => {
+    removeBtn.addEventListener('click', () => {
         divEle.remove()
         updateStorage()
         // e.target.parentNode.parentNode.remove()
@@ -65,7 +61,6 @@ function addNote(note = {}) {
 }
 
 function updateStorage() {
-    // console.log(document.querySelectorAll('.singleNote'))
     data = Array.from(document.querySelectorAll('.singleNote')).map(noteDiv => {
         let textArea = noteDiv.querySelector('textarea').value
         let date = noteDiv.querySelector('.date').textContent
